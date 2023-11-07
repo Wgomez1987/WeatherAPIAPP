af0eed02d0d8b3ecfbd5ba91f41b46ad9e8097f9
Wgomez1987 <gomez.william.a1@gmail.com>
2023-11-07 14:23:45 -0800

    Final Commit

--- Assets/script.js
+++ Assets/script.js
@@ -2,7 +2,6 @@ document.addEventListener('DOMContentLoaded', function() {
     const searchBtn = document.getElementById('searchBtn');
     const cityInput = document.getElementById('city');
     const infoBox = document.getElementById('infoBox');
-    const forecast = document.getElementById('forecast');
 
     searchBtn.addEventListener('click', function(event) {
         event.preventDefault(); // Prevent form submission which refreshes the page
@@ -13,7 +12,7 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     function getWeather(city) {
-        const apiKey = 'YOUR_API_KEY'; // Replace with your actual API key
+        const apiKey = '4f90d0c7f4387060b64058b54efe76db';
         const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
         fetch(url)
@@ -25,8 +24,10 @@ document.addEventListener('DOMContentLoaded', function() {
         })
         .then(data => {
             updateInfoBox(data);
-            // For the forecast, you would typically call another API endpoint
-            // getForecast(city);
+            // Once we have the current weather, we can also get the forecast using the coordinates
+            const lat = data.coord.lat;
+            const lon = data.coord.lon;
+            getForecast(lat, lon); // Call the getForecast function with the obtained latitude and longitude
         })
         .catch(error => {
             console.error('Error fetching weather:', error);
@@ -37,9 +38,51 @@ document.addEventListener('DOMContentLoaded', function() {
     function updateInfoBox(data) {
         infoBox.innerHTML = `
             <h2>${data.name}</h2>
-            <p>Temperature: ${data.main.temp} °C</p>
+            <p>Temperature: ${data.main.temp} °F</p>
             <p>Weather: ${data.weather[0].description}</p>
             <p>Humidity: ${data.main.humidity}%</p>
             <p>Wind Speed: ${data.wind.speed} m/s</p>
         `;
     }
+
+    function getForecast(lat, lon) {
+        const apiKey = '4f90d0c7f4387060b64058b54efe76db'; // Your API key
+        const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+    
+        fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            updateForecast(data);
+        })
+        .catch(error => {
+            console.error('Error fetching forecast:', error);
+        });
+    }
+    
+    function updateForecast(data) {
+        // Loop through the forecast for the next five days
+        for (let i = 0; i < 5; i++) {
+            const forecastData = data.list[i * 8]; // Get the data point for approximately every 24 hours
+            const dateTime = new Date(forecastData.dt * 1000);
+            const date = dateTime.toLocaleDateString();
+            const temp = forecastData.main.temp;
+            const weather = forecastData.weather[0].description;
+            const iconCode = forecastData.weather[0].icon;
+            const iconUrl = `http://openweathermap.org/img/w/${iconCode}.png`;
+
+            // Update the corresponding forecast box
+            const forecastDiv = document.getElementById(`forecast-day-${i+1}`);
+            forecastDiv.innerHTML = `
+                <h4>${date}</h4>
+                <img src="${iconUrl}" alt="Weather icon">
+                <p>Temp: ${temp} °C</p>
+                <p>${weather}</p>
+            `;
+        }
+    }
+});
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # WeatherAPIAPP
+```
+I rendered using JS all data from API.
+```
+I was able to get localstorage to work.
+```
+I couldn't get it to transition from C - F.
+
+Thank you.
